d6c6bbaca59e49cc4593c9e04aabcb0cd02aeb42
subratpn <subratpn@gmail.com>
2020-02-18 13:52:50 +0530

    Commit

--- src/main/java/store/redmeat/models/Product.java
+++ src/main/java/store/redmeat/models/Product.java
@@ -18,8 +18,61 @@ public class Product {
     @DBRef
     private Set<Image> images;
     @DBRef
-    private ProductSupplier productSupplier;
+    private Set<ProductSupplier> productSuppliers;
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public Set<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(Set<Image> images) {
+        this.images = images;
+    }
+
+    public Set<ProductSupplier> getProductSuppliers() {
+        return productSuppliers;
+    }
+
+    public void setProductSuppliers(Set<ProductSupplier> productSuppliers) {
+        this.productSuppliers = productSuppliers;
+    }
 }
